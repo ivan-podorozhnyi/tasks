d422ea1cbdf34f8a5d9cb8c6bc2d1d9ca3f5187f
Ivan Podorozhnyi <ivan.podorozhnyi@gmail.com>
2019-02-12 23:00:22 +0200

    Update main method

--- 01/src/app.java
+++ 01/src/app.java
@@ -1,11 +1,9 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
-import Models.SelectionSorter;
-import Models.MergeSorter;
-import Models.BubbleSorter;
-import Models.Sorter;
+import Models.*;
 
 
 public class app {
@@ -13,18 +11,23 @@ public class app {
         int[] ints = {1, 4, 5, 12, 6};
 
         Sorter selectionSorter = new SelectionSorter();
-        Sorter mergeSorter = new MergeSorter();
+        Sorter quickSorter = new QuickSorter();
         Sorter bubbleSorter = new BubbleSorter();
 
         List<Sorter> sortersList = new ArrayList<>();
 
         sortersList.add(selectionSorter);
         sortersList.add(bubbleSorter);
-        sortersList.add(mergeSorter);
+        sortersList.add(quickSorter);
 
-        for (Sorter sorter: sortersList) {
+        for (Sorter sorter : sortersList) {
             sorter.sort(ints);
             System.out.println(Arrays.toString(ints));
         }
+
+        Searcher binarySearcher = new BinarySearcher();
+        int randomIndex =new Random().nextInt(ints.length);
+
+        binarySearcher.search(ints, ints[randomIndex]);
     }
 }
